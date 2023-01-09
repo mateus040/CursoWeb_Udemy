86af6a6108da1f2458cbfbe34402f2ecea8f8afa
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-01-09 13:19:16 -0300

    adicionando comentários

--- JavaScript/Incluindo_JavaScript_HTML5/script.js
+++ JavaScript/Incluindo_JavaScript_HTML5/script.js
@@ -1,2 +1,8 @@
+// Mostra um alerta ao carregar a página 
 alert('Olá, seja bem vindo ao curso!')
+
+
+/*  Seleciona um elemento no DOM
+    Atualiza o valor desse elemento com uma string 
+*/
 document.getElementById('nome').value = 'Oi'
