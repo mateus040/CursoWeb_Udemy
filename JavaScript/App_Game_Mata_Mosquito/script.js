d48af7716327609f5b8b1a0a71f0128552a17eff
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-02-01 14:01:02 -0300

    criando posições randômicas

--- JavaScript/App_Game_Mata_Mosquito/script.js
+++ JavaScript/App_Game_Mata_Mosquito/script.js
@@ -3,11 +3,32 @@ var largura = 0
 
 function ajustaTamanhoPalcoJogo()
 {
-    altura = window.innerHeight
-    largura = window.innerWidth
+    altura = window.innerHeight // Valores aleatórios
+    largura = window.innerWidth // Valores aleatórios
 
-    console.log(altura, largura)
+    console.log(largura, altura)
 }
 
 ajustaTamanhoPalcoJogo()
+
+function posicaoRandomica()
+{
+    var posicaoX = Math.floor(Math.random() * largura) - 90 // Gerando valor aleatório de acordo com a largura e altura do "palco"
+    var posicaoY = Math.floor(Math.random() * altura) - 90 // Gerando valor aleatório de acordo com a largura e altura do "palco"
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+
+    // Criando um elemento HTML (em forma programática)
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px' // Formando a coordenada em pixel
+    mosquito.style.top = posicaoY + 'px' // Formando a coordenada em pixel
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito) // Adicionando um "filho" ao body
+}
 
